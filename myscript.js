481f75a5ceeec0f38fd60895cce6b2576d1af33b
shamsheer974 <70147850+shamsheer974@users.noreply.github.com>
2020-09-03 15:36:38 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,3 @@
+function myFunction(){
+document.getElementById("demo").innerHTML="javascript is a scripting language.In javascriptwe can change content,attributes values ,style and content hide of html"
+}
